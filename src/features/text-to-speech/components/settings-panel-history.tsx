2c12760aaeead6686129762967d7c1e0e28ee7d0
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-03-07 13:07:27 +0530

    text to speech ui

---
+++ src/features/text-to-speech/components/settings-panel-history.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+
+import { formatDistanceToNow } from "date-fns";
+import { AudioLines, AudioWaveform, Clock } from "lucide-react";
+import Link from "next/link";
+
+export function SettingsPanelHistory() {
+
+
+ 
+
+  return (
+    <div className="flex flex-col gap-1 p-2">
+      <h1>Settings History</h1>
+    </div>
+  )
+};
